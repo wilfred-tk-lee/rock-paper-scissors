353b334ca0045af6e4d1093a20e47ef9eb79e729
wilfred-tk-lee <78968590+wilfred-tk-lee@users.noreply.github.com>
2021-02-12 22:41:04 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let Hand = 0
+input.onGesture(Gesture.Shake, function () {
+    Hand = randint(1, 3)
+    if (Hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (Hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
